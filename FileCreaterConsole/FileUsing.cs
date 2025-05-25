e53cf37e71556ee7701a8917dd08940983e70332
salaga999999999 <salaga999999999@gmail.com>
2025-05-25 15:44:23 +0700

    Task 1 Complite

---
+++ FileCreaterConsole/FileUsing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCreaterConsole
+{
+    internal class FileUsing
+    {
+        public static DirectoryInfo CreationDir(string dirPath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(dirPath);
+            if (!dir.Exists)
+            {
+                try
+                {
+                    dir.Create();
+                    Console.WriteLine($"Directory {dirPath} created");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+            return dir;
+        }
+    }
+}
---
+++ FileCreaterConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FileCreaterConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            var TestDir1 = FileUsing.CreationDir(@"c:\Otus\TestDir1");
+            Console.WriteLine(TestDir1.FullName);
+            if (TestDir1 is null) return;
+            var TestDir2 = FileUsing.CreationDir(@"c:\Otus\TestDir2");
+            Console.WriteLine(TestDir2.FullName);
+            if (TestDir2 is null) return;
+
+            //Task 2
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
